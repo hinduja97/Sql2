75556070704b95561a4f861e776d1f1ef8685f65
Hinduja Cheela <hindujacheela@Mac.attlocal.net>
2025-05-22 16:43:52 -0500

    Added Fouth problem

--- Problem 4.sql
+++ Problem 4.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
 
-
-with cte as (
-select d.name as Department,e.name as Employee,dense_rank() over (partition by d.name order by Salary desc) as dense_sal
-from employee e
-join department d on e.departmentId=d.id
-order by salary desc)
-
-select Department,Employee,dense_sal
-from cte ;
+with cte as(
+    select e.id,e.name as Employee,e.salary as Salary,d.name as Department,dense_rank() over(partition by d.   name order by e.salary desc) as dens_col
+    from employee e
+    join Department d on e.departmentId = d.id)
+    
+    select Department,Employee,Salary
+    from cte
+    where dens_col <=3;
